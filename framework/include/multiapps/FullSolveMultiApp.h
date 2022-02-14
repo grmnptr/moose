ff8cff5ade5a48e0271f99eec7602ad276581599
Peter German <grmnptr@gmail.com>
2022-02-14 14:09:03 -0700

    Add new approach, modules seemingly pass. (#20262)

--- framework/include/multiapps/FullSolveMultiApp.h
+++ framework/include/multiapps/FullSolveMultiApp.h
@@ -30,6 +30,8 @@ public:
 
   virtual void initialSetup() override;
 
+  virtual void initialSetup(bool solved) override;
+
   virtual bool solveStep(Real dt, Real target_time, bool auto_advance = true) override;
 
   virtual void finalize() override
