200ce39d1315a9c5552b02d994eb9541fd668e42
Peter German <grmnptr@gmail.com>
2023-07-05 18:01:12 -0600

    Add initial version of drag postprocessor. (#24909)

---
+++ modules/navier_stokes/include/postprocessors/DragCoefficient.h
@@ -0,0 +1,49 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+// MOOSE includes
+#include "SideIntegralPostprocessor.h"
+#include "MathFVUtils.h"
+
+/**
+ * This postprocessor which computes the drag coefficient on a surface
+ * defined as:
+ *
+ * $C_D = \int\limits_S (\sigma \vec{n}) \cdot \vec{e} dS$
+ *
+ * where $\sigma$ is the Cauchy stress tensor
+ */
+class DragCoefficient : public SideIntegralPostprocessor
+{
+public:
+  static InputParameters validParams();
+
+  DragCoefficient(const InputParameters & parameters);
+
+protected:
+  Real computeFaceInfoIntegral(const FaceInfo * fi) override;
+
+  Real computeQpIntegral() override;
+
+  /// Velocity components
+  const Moose::Functor<Real> & _vel_x;
+  const Moose::Functor<Real> * _vel_y;
+  const Moose::Functor<Real> * _vel_z;
+
+  /// The dynamic viscosity
+  const Moose::Functor<Real> & _mu;
+
+  /// Pressure field
+  const Moose::Functor<Real> & _pressure;
+
+  /// The direction in which the drag/lift is measured
+  const RealVectorValue _direction;
+};
---
+++ modules/navier_stokes/src/postprocessors/DragCoefficient.C
@@ -0,0 +1,94 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "DragCoefficient.h"
+#include "MathFVUtils.h"
+#include "NSFVUtils.h"
+#include "NS.h"
+
+#include <cmath>
+
+registerMooseObject("NavierStokesApp", DragCoefficient);
+
+InputParameters
+DragCoefficient::validParams()
+{
+  InputParameters params = SideIntegralPostprocessor::validParams();
+  params.addClassDescription(
+      "Computes the drag coefficient on a surface. Depending on the principal direction, one can "
+      "use this object for the computation of the lift coefficient as well.");
+  params.addRequiredParam<MooseFunctorName>("vel_x", "The velocity in direction x.");
+  params.addParam<MooseFunctorName>("vel_y", "The velocity in direction y.");
+  params.addParam<MooseFunctorName>("vel_z", "The velocity in direction z.");
+  params.addRequiredParam<MooseFunctorName>(NS::mu, "The dynamic viscosity.");
+  params.addRequiredParam<MooseFunctorName>(NS::pressure, "The pressure functor.");
+  params.addRequiredParam<RealVectorValue>("principal_direction",
+                                           "The direction in which the drag is computed. This can "
+                                           "be used to compute the lift coefficient as well.");
+  return params;
+}
+
+DragCoefficient::DragCoefficient(const InputParameters & parameters)
+  : SideIntegralPostprocessor(parameters),
+    _vel_x(getFunctor<Real>("vel_x")),
+    _vel_y(isParamValid("vel_y") ? &getFunctor<Real>("vel_y") : nullptr),
+    _vel_z(isParamValid("vel_z") ? &getFunctor<Real>("vel_z") : nullptr),
+    _mu(getFunctor<Real>(NS::mu)),
+    _pressure(getFunctor<Real>(NS::pressure)),
+    _direction(getParam<RealVectorValue>("principal_direction"))
+{
+  _qp_integration = !getFieldVar("vel_x", 0)->isFV();
+  if (_qp_integration)
+    mooseError("Drag coefficient computation is only supported for finite volume variables!");
+}
+
+Real
+DragCoefficient::computeFaceInfoIntegral(const FaceInfo * fi)
+{
+  mooseAssert(fi, "We should have a face info in " + name());
+  const auto state = determineState();
+  const auto face_arg =
+      Moose::FaceArg({fi, Moose::FV::LimiterType::CentralDifference, true, false, nullptr});
+
+  RealTensorValue velocity_gradient;
+  RealTensorValue pressure_term;
+  Real pressure = _pressure(face_arg, state);
+  Real mu = _mu(face_arg, state);
+  if (_mesh.dimension() == 1)
+  {
+    const auto & grad_u = _vel_x.gradient(face_arg, state);
+    velocity_gradient = RealTensorValue(grad_u);
+    pressure_term(0, 0) = -pressure;
+  }
+  else if (_mesh.dimension() == 2)
+  {
+    // const auto & grad_v = _v_var->adGradSln(face_info, skewness_correction);
+    const auto & grad_u = _vel_x.gradient(face_arg, state);
+    const auto & grad_v = _vel_y->gradient(face_arg, state);
+    velocity_gradient = RealTensorValue(grad_u, grad_v);
+    pressure_term(1, 1) = -pressure;
+  }
+  else // if (_dim == 3)
+  {
+    const auto & grad_u = _vel_x.gradient(face_arg, state);
+    const auto & grad_v = _vel_y->gradient(face_arg, state);
+    const auto & grad_w = _vel_z->gradient(face_arg, state);
+    velocity_gradient = RealTensorValue(grad_u, grad_v, grad_w);
+    pressure_term(2, 2) = -pressure;
+  }
+
+  return (mu * (velocity_gradient + velocity_gradient.transpose()) + pressure_term) * fi->normal() *
+         _direction;
+}
+
+Real
+DragCoefficient::computeQpIntegral()
+{
+  return 0.0;
+}
