c9bb9401b062a79fee162cb192b0dc2f52eab8e7
Peter German <grmnptr@gmail.com>
2023-10-12 17:58:14 -0600

    Add actions for LinearFVKernel. (#25722)

--- framework/src/base/Moose.C
+++ framework/src/base/Moose.C
@@ -542,6 +542,8 @@ associateSyntaxInner(Syntax & syntax, ActionFactory & /*action_factory*/)
   registerSyntax("AddFVInterfaceKernelAction", "FVInterfaceKernels/*");
   registerSyntax("CheckFVBCAction", "FVBCs");
 
+  registerSyntax("AddLinearFVKernelAction", "LinearFVKernels/*");
+
   registerSyntax("AddInterfaceKernelAction", "InterfaceKernels/*");
 
   registerSyntax("AddConstraintAction", "Constraints/*");
